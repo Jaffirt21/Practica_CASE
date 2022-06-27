699e2dbcb99f7820745cc4746073b4fefc74adff
Jaffirt21 <zuriel592@gmail.com>
2022-06-27 12:48:33 -0500

    Repositorio CASE

---
+++ decinalBinario.py
@@ -0,0 +1,16 @@
+#Convertimos el entero 11 a binario
+bin(11)
+#Nos devuelve una cadena.
+'0b1011'
+#Paea convertir el numero en un entero.
+int(bin(11)[2:])
+1011
+
+
+
+
+b = 1011
+#Convertimos el entero en una cadena y despeas lo pasamos a binario.
+#Base 2.
+print int(str(b), 2)
+11
